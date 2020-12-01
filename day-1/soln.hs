f543075825e8664772df150616973fa55f57a555
Mitchell Burton <mitch.b45@gmail.com>
2020-11-30 22:06:33 -0800

    fix haskell indentation

--- day-1/soln.hs
+++ day-1/soln.hs
@@ -3,13 +3,12 @@ import Data.List
 main = do
     rawInput <- readFile "input.txt"
     let input = [read i :: Int | i <- lines rawInput] in
-        do
-            print $ soln1 input
-            print $ soln2 input
+        do print $ soln1 input
+           print $ soln2 input
 
 soln1 input = head [x * y | (x:ys) <- tails input, y <- ys, x + y == 2020]
 
 soln2 input = head [x * y * z | (x:ys) <- tails input,
-                           (y:zs) <- tails ys,
-                           z <- zs,
-                           x + y + z == 2020]
+                                (y:zs) <- tails ys,
+                                z <- zs,
+                                x + y + z == 2020]
